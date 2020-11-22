34e47c1b90ccc7e8885b139ac3800b2b96305e98
Philip Tong <philiptong@users.noreply.github.com>
2020-11-22 15:16:38 +0000

    Initial project

---
+++ src/main/java/com/philipt/nearby/App.java
@@ -0,0 +1,13 @@
+package com.philipt.nearby;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/philipt/nearby/model/Location.java
@@ -0,0 +1,10 @@
+package com.philipt.nearby.model;
+
+import lombok.Data;
+
+@Data
+public class Location {
+
+    private double latitude;
+    private double longitude;
+}
